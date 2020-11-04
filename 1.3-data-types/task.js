50fbe263d19c5f12f6017cd2e87f3c487076193d
Anna Nosova <anna.nosova.int@gmail.com>
2020-11-04 15:28:01 +0300

    first commit wrappers-for-primitives

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,34 @@
+'use strict'
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    let currentDate = new Date ();
+    let monthsAmount = Math.round((Math.abs(date.getTime() - currentDate.getTime())) / (1000 * 3600 * 24 * 30.5));
+
+    
+    let mortgageData = {
+        percent: parseInt(percent),
+        contribution: parseInt(contribution),
+        amount: parseInt(amount),
+        monthsAmount: monthsAmount,
+    }
+
+
+    for (let key in Object(mortgageData)) {
+        if (isNaN(mortgageData[key]) || mortgageData[key] === undefined) {
+            return `Параметр ${key} содержит неправильное значение ${mortgageData[key]}`;
+        } 
+    }
+
+    mortgageData.index = mortgageData.percent / 100 / 12;
+    console.log(mortgageData.index);
+    let monthlyPayment = (mortgageData.amount - mortgageData.contribution) * (mortgageData.index + mortgageData.index / (((1 + mortgageData.index) ** mortgageData.monthsAmount) - 1));
+    let totalAmount = new Number((monthlyPayment * mortgageData.monthsAmount).toFixed(2));
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    if (name === '' || name == undefined) {
+        name = 'Аноним';
+    }
+    return `Привет, мир! Меня зовут ${name}.`;
 }
