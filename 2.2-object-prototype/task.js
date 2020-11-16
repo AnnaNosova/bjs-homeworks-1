d658c09d6c6ba11cfbf001387d4780c2ac23b150
Anna Nosova <anna.nosova.int@gmail.com>
2020-11-16 22:34:50 +0300

    first commit object-prototype

--- 2.2-object-prototype/task.js
+++ 2.2-object-prototype/task.js
@@ -1,11 +1,43 @@
-//String.prototype.isPalindrome - для задачи №1
+'use strict';
+
+String.prototype.isPalindrome = function() {
+    let str = this.toLowerCase().replace(/[^а-я]/g,"");
+    if (str.length > 0) {
+        for (let i = 0; i < str.length/2; i++) {
+            if (str[i] !== str[str.length - i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    } else {
+        return Error;
+    }
+}
+
 
 function getAverageMark(marks) {
-    // код для задачи №2 писать здесь
-    // return averageMark
+    if (marks.length === 0) {
+        return 0;
+    } else {
+        let marksSum = 0;
+        for (let i = 0; i < marks.length; i++) {
+            marksSum += Number(marks[i]);
+        }
+        let averageMark = Math.round(marksSum / marks.length);
+
+        return averageMark;
+    }
 }
 
 function checkBirthday(birthday) {
-    // код для задачи №3 писать здесь
-    // return verdict
+    let now = Date.now();
+    let diff = now - Number(new Date(birthday));
+    let age = diff / (365.25*24*60*60*1000);
+    let verdict;
+    if (age < 18) {
+        verdict = false;
+    } else {
+        verdict = true;
+    }
+    return verdict;
 }
